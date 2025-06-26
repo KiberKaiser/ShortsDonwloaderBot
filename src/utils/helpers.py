9243ff5969b510d6f6836c7d293a09989d75ed9f
CyberKaiser <48377917+KiberKaiser@users.noreply.github.com>
2025-06-26 17:00:25 +0300

    Delete src/utils directory

--- src/utils/helpers.py
+++
@@ -1,7 +0,0 @@
-def is_valid_url(url):
-    return url.startswith(("http://", "https://"))
-
-def extract_video_id(url):
-    return url.split("/")[-1] if url else None
-def handle_error(error_message):
-    print(f"Error: {error_message}")
