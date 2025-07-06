8cd977c59e73432f3b778ce4b1bad69c5d4fe4b5
CyberKaiser <48377917+KiberKaiser@users.noreply.github.com>
2025-07-07 00:34:07 +0300

    guzlan update

--- src/services/tiktok_downloader.py
+++ src/services/tiktok_downloader.py
@@ -17,3 +17,24 @@ def download_tiktok_video(url: str) -> str | None:
     except Exception as e:
         print(f"Ошибка скачивания видео из TikTok: {e}")
         return None
+
+def download_tiktok_audio(url: str) -> str | None:
+    output_path = "tiktok_audio"
+    ydl_opts = {
+        'outtmpl': output_path,
+        'format': 'bestaudio[ext=m4a]/bestaudio/best[ext=m4a]/best',
+        'quiet': True,
+        'no_warnings': True,
+        'noplaylist': True,
+    }
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=False)
+            final_filename = ydl.prepare_filename(info)
+            
+            ydl.download([url])
+            
+            return final_filename if os.path.exists(final_filename) else None
+    except Exception as e:
+        print(f"Ошибка скачивания аудио из TikTok: {e}")
+        return None
